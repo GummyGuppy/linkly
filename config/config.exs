6cab9744a5b9ce39ac420274ca9eeeb58b788223
gummyguppy <alphastack.dev@gmail.com>
2024-01-04 18:46:44 -0600

    added config directory

---
+++ config/config.exs
@@ -0,0 +1,5 @@
+import Mix.Config
+
+config :linkly, :ecto_repos, {Linkly.Repo}
+
+import_config "#{Mix.env}.exs"
---
+++ config/dev.exs
@@ -0,0 +1,8 @@
+use Mix.config
+
+config, :linkly, Linkly.Repo,
+
+  username: "postgres",
+  password: "postgres",
+  database: "linkly_dev",
+  hostname: "localhost"
